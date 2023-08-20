d72fc8246e2cbf9a3de3e5921f2b7cbf0bd5d343
prajwal-26 <127192880+prajwal-26@users.noreply.github.com>
2023-08-21 00:02:52 +0530

    Added solution - LeetHub

---
+++ Find the median - GFG/find-the-median.js
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = readLine().trim().split(" ").map((x) => parseInt(x));
+        let obj = new Solution();
+        let res=obj.find_median(arr);
+        console.log(res);
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+*/
+
+class Solution {
+
+    find_median(arr){
+        //sort the arr in desending order
+        arr.sort((a,b) => a - b)
+        //find the mid value of an array
+        const mid = Math.floor (arr.length / 2);
+        // if mid value is even sum of two mid numbers else return mid number
+        if(arr.length % 2 === 0){
+            return Math.floor((arr[mid - 1] + arr [mid]) / 2)
+            
+        }
+        else{
+            return arr[mid]
+        }
+        
+        
+    }
+}
+const solution = new Solution
+const arr = [90 , 100,78,89,67]
+const final = solution.find_median(arr)
+// console.log(arr.length)
+// console.log(final)
