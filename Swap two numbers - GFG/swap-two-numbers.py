409cb9fdb3f932f4d0d31b646529ad86b3b59e55
prajwal-26 <127192880+prajwal-26@users.noreply.github.com>
2023-08-21 01:04:21 +0530

    Added solution - LeetHub

---
+++ Swap two numbers - GFG/swap-two-numbers.py
@@ -0,0 +1,27 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    def get(self, a, b):
+        #code here
+        a ,b = b, a
+        return a,b
+        
+        solution = Solution();
+        a = 5
+        b = 6
+        a , b = solution.get(a,b)
+
+#{ 
+ # Driver Code Starts.
+if __name__ == "__main__":
+	t = int (input ())
+	for tc in range (t):
+		a,b = map(int,input().strip().split())
+		ob = Solution()
+		ans=ob.get(a,b)
+		print(str(ans[0])+" "+str(ans[1]))
+# } Driver Code Ends
