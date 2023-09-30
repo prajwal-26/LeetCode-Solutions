c63990893cffe68a9df193aa2c5d623d1a425a7d
prajwal patil <127192880+prajwal-26@users.noreply.github.com>
2023-09-30 18:14:50 +0530

    Time: 51 ms (68.34%), Space: 42.6 MB (27.88%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -2,9 +2,9 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var runningSum = function(nums) {
+function runningSum (nums) {
     for (let i = 1 ; i < nums.length ; i++ ){
-        nums[i]  = nums[i] + nums[i - 1];
+        nums[i] += nums[i - 1];
     }
     return nums;
 };
