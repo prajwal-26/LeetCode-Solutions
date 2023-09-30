08e8dbbee21789fb3bec79706176ef53c81630b5
prajwal patil <127192880+prajwal-26@users.noreply.github.com>
2023-09-30 21:36:46 +0530

    Time: 48 ms (75.83%), Space: 42.5 MB (15.70%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function(accounts) {
+     let maxwealth = 0;
+        for(let i = 0 ; i < accounts.length ; i++){
+            let currentwealth = 0;
+            for(let j = 0 ; j < accounts[i].length ; j++){
+                currentwealth += accounts[i][j];
+            }
+            maxwealth = Math.max(maxwealth,currentwealth);
+        }
+    return maxwealth;
+};
