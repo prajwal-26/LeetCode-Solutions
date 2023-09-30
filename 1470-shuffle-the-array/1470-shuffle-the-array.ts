db692f71feb6d97d8957681f22559dcf746fe8ca
prajwal patil <127192880+prajwal-26@users.noreply.github.com>
2023-09-30 22:23:53 +0530

    Time: 67 ms (40.96%), Space: 44.6 MB (76.01%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.ts
+++ 1470-shuffle-the-array/1470-shuffle-the-array.ts
@@ -1,5 +1,5 @@
 function shuffle(nums:number[], n:number):number[] {
-    let items:number[] = [];
+    let items:number[] = new Array(nums.length);
     let index:number = 0 ;
     for(let i = 0; i < n; i++){
         items[index++] = nums[i];
