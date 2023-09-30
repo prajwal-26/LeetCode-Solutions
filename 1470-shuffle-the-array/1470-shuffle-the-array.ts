127a973502a9e3069a7ea8a925bb442fa72ba301
prajwal patil <127192880+prajwal-26@users.noreply.github.com>
2023-09-30 22:25:33 +0530

    Time: 72 ms (14.76%), Space: 43.9 MB (100.00%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.ts
+++ 1470-shuffle-the-array/1470-shuffle-the-array.ts
@@ -1,9 +1,8 @@
-function shuffle(nums:number[], n:number):number[] {
-    let items:number[] = new Array(nums.length);
-    let index:number = 0 ;
-    for(let i = 0; i < n; i++){
-        items[index++] = nums[i];
-        items[index++] = nums[i + n];
-    } 
-    return items
-};
+function shuffle(nums: number[], n: number): number[] {
+    const items: number[] = [];
+    for (let i = 0; i < n; i++) {
+        items.push(nums[i]);
+        items.push(nums[i + n]);
+    }
+    return items;
+}
